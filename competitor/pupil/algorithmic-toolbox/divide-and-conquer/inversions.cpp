f7051d7f8f8a1e32621ae549c4594d700db700b1
Amgd2112 <amgad.hussein99@gmail.com>
2022-01-01 00:33:50 +0200

    day 42

--- competitor/pupil/algorithmic-toolbox/divide-and-conquer/inversions.cpp
+++ competitor/pupil/algorithmic-toolbox/divide-and-conquer/inversions.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 #define FIO freopen("input.txt", "r", stdin);
 
-void merge(int a[], int const &left, int const &mid, int const &right) {
+int merge(int a[], int const &left, int const &mid, int const &right) {
   int const n = mid - left + 1;
   int const m = right - mid;
 
@@ -16,26 +16,31 @@ void merge(int a[], int const &left, int const &mid, int const &right) {
   for (int i = 0; i < n; i++) b[i] = a[left + i];
   for (int j = 0; j < m; j++) c[j] = a[mid + 1 + j];
 
-  int i = 0, j = 0, k = left;
+  int i = 0, j = 0, k = left, inversions = 0;
   while (i < n && j < m) {
     if (b[i] <= c[j]) {
       a[k++] = b[i++];
     } else {
+      inversions += n - i;
       a[k++] = c[j++];
     }
   }
 
   while (i < n) a[k++] = b[i++];
   while (j < m) a[k++] = c[j++];
+
+  return inversions;
 }
 
-void merge_sort(int a[], int const &begin, int const &end) {
-  if (begin >= end) return;
+int merge_sort(int a[], int const &begin, int const &end) {
+  if (begin >= end) return 0;
 
   int const mid = begin + (end - begin) / 2;
-  merge_sort(a, begin, mid);
-  merge_sort(a, mid + 1, end);
-  merge(a, begin, mid, end);
+  int inversions = 0;
+  inversions += merge_sort(a, begin, mid);
+  inversions += merge_sort(a, mid + 1, end);
+  inversions += merge(a, begin, mid, end);
+  return inversions;
 }
 
 int main(int argc, char const *argv[]) {
@@ -48,8 +53,8 @@ int main(int argc, char const *argv[]) {
   int a[n];
   for (int &ai : a) scanf("%d", &ai);
 
-  merge_sort(a, 0, n);
-  for (int ai : a) printf("%d ", ai);
+  int x = merge_sort(a, 0, n);
+  printf("%d\n", x);
 
   return 0;
 }

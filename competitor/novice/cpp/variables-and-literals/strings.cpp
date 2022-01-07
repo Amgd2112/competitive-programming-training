26411efa16bf6d72b0d0339acbb09d9332fb20d4
Amgd2112 <amgad.hussein99@gmail.com>
2022-01-07 14:28:58 +0200

    characters and strings

--- competitor/novice/cpp/variables-and-literals/strings.cpp
+++ competitor/novice/cpp/variables-and-literals/strings.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   cout << "Hello, World!\n";
 
+  /*
+   * String is an array of characters.
+   * The last element in the array is equal to '\0', which is a null character.
+   */
   char name[100] = "Hanan Mohamed";
   // ['H', 'a', 'n', 'a', 'n', ' ', 'M', 'o', 'h', 'a', 'm', 'e', 'd', '\0', '']
   cout << name << '\n';
